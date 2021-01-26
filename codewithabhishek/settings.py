db2d5bff5ce8ba3487642bbdf8b3ff0d7445f638
abhisljh <abhi.sljh@gmail.com>
2021-01-26 05:36:04 +0530

    Commit

--- codewithabhishek/settings.py
+++ codewithabhishek/settings.py
@@ -44,11 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courses',
-    'crispy_forms',
+    'courses'
 ]
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
